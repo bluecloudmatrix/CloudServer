a3bfdcdfd1168c99761d37eb5a27454dddd66038
Monkey.D.Luffy <bluecloudmatrix@gmail.com>
2016-08-01 11:56:58 +0800

    push images

--- images.js
+++ images.js
@@ -2,16 +2,16 @@
  * Created by qiushan on 5/31/2016.
  */
 var entries = [
-    {"id":0001, "name":"MongoDB", "image":"weare.pub/mongodb:3.0", "picture":"mongodb.png", "description":""},
-    {"id":0002, "name":"Node.js", "image":"mymongodb", "picture":"nodejs.png", "description":""},
-    {"id":0003, "name":"cadvisor", "image":"mymongodb", "picture":"cadvisor.png", "description":""},
-    {"id":0004, "name":"mysql", "image":"mymongodb", "picture":"mysql.png", "description":""},
-    {"id":0005, "name":"nginx", "image":"mymongodb", "picture":"nginx.png", "description":""},
-    {"id":0006, "name":"PHP", "image":"mymongodb", "picture":"PHP.png", "description":""},
-    {"id":0007, "name":"haproxy", "image":"mymongodb", "picture":"haproxy.png", "description":""},
-    {"id":0008, "name":"Ubuntu", "image":"mymongodb", "picture":"Ubuntu.png", "description":""},
-    {"id":0009, "name":"wordpress", "image":"mymongodb", "picture":"wordpress.png", "description":""},
-    {"id":0010, "name":"Spark", "image":"mymongodb", "picture":"Spark.jpg", "description":""}
+    {"id":0001, "name":"MongoDB", "image":"weare.pub/mongodb:latest", "picture":"mongodb.png", "description":""},
+    {"id":0002, "name":"Node.js", "image":"weare.pub/node:latest", "picture":"nodejs.png", "description":""},
+    {"id":0003, "name":"cadvisor", "image":"weare.pub/cadvisor:latest", "picture":"cadvisor.png", "description":""},
+    {"id":0004, "name":"mysql", "image":"weare.pub/mysql:latest", "picture":"mysql.png", "description":""},
+    {"id":0005, "name":"nginx", "image":"weare.pub/nginx:latest", "picture":"nginx.png", "description":""},
+    {"id":0006, "name":"PHP", "image":"weare.pub/php:latest", "picture":"PHP.png", "description":""},
+    {"id":0007, "name":"haproxy", "image":"weare.pub/haproxy:latest", "picture":"haproxy.png", "description":""},
+    {"id":0008, "name":"Ubuntu", "image":"weare.pub/ubuntu:latest", "picture":"Ubuntu.png", "description":""},
+    {"id":0009, "name":"wordpress", "image":"weare.pub/wordpress:latest", "picture":"wordpress.png", "description":""},
+    {"id":0010, "name":"Spark", "image":"weare.pub/spark-standalone:latest", "picture":"Spark.jpg", "description":""}
 ];
 
 exports.getImagesEntries = function(){
