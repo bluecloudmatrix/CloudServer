a5d11614c05b24b854fe854a2a320ee672128111
Monkey.D.Luffy <bluecloudmatrix@gmail.com>
2016-08-08 17:29:09 +0800

    square update

--- images.js
+++ images.js
@@ -7,11 +7,7 @@ var entries = [
     {"id":0003, "name":"cadvisor", "image":"weare.pub/cadvisor:latest", "picture":"cadvisor.png", "description":""},
     {"id":0004, "name":"mysql", "image":"weare.pub/mysql:latest", "picture":"mysql.png", "description":""},
     {"id":0005, "name":"nginx", "image":"weare.pub/nginx:latest", "picture":"nginx.png", "description":""},
-    {"id":0006, "name":"PHP", "image":"weare.pub/php:latest", "picture":"PHP.png", "description":""},
-    {"id":0007, "name":"haproxy", "image":"weare.pub/haproxy:latest", "picture":"haproxy.png", "description":""},
-    {"id":0008, "name":"Ubuntu", "image":"weare.pub/ubuntu:latest", "picture":"Ubuntu.png", "description":""},
-    {"id":0009, "name":"wordpress", "image":"weare.pub/wordpress:latest", "picture":"wordpress.png", "description":""},
-    {"id":0010, "name":"Spark", "image":"weare.pub/spark-standalone:latest", "picture":"Spark.jpg", "description":""}
+    {"id":0008, "name":"Ubuntu", "image":"weare.pub/ubuntu:latest", "picture":"Ubuntu.png", "description":""}
 ];
 
 exports.getImagesEntries = function(){
